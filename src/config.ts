444028e9e4ba3c598cc9114a6a16329dd7528ead
Rechcel Toledo <rechceltoledo@gmail.com>
2025-06-27 21:23:57 +0800

    added: affluent backend

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 // Centralized configuration for the frontend
 // Update BACKEND_URL here to change it everywhere
 
-export const BACKEND_URL = "http://localhost:4000"; // Change this as needed
+//export const BACKEND_URL = "http://localhost:4000"; // Change this as needed
+export const BACKEND_URL = "https://affluent-backend.vercel.app"; // Change this as needed
