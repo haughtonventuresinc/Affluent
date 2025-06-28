2fb2f4023e56e9b6472560ff824435e2633931d8
Rechcel Toledo <rechceltoledo@gmail.com>
2025-06-29 02:39:57 +0800

    added: railway backend

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Centralized configuration for the frontend
 // Update BACKEND_URL here to change it everywhere
 
-export const BACKEND_URL = "http://localhost:4000"; // Change this as needed
-//export const BACKEND_URL = "https://affluent-production.up.railway.app"; // Change this as needed
+//export const BACKEND_URL = "http://localhost:4000"; // Change this as needed
+export const BACKEND_URL = "https://affluent-production.up.railway.app"; // Change this as needed
